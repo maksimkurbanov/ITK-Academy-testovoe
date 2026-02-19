7283354c3c3a0482c1e03989f29c3733f127eea7
Maksim <mkurbanov2@gmail.com>
2026-02-19 14:56:48 +0300

    setup_and_teardown cleanup

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,6 @@
 import pytest
-from sqlalchemy.ext.asyncio import create_async_engine
 from src.api.v1.wallets.models import Base
-from src.database import SQLALCHEMY_DATABASE_URL
+from tests.conftest import engine
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -11,12 +10,8 @@ async def setup_and_teardown():
     tables based on SQLAlchemy models, so each test maintains
     maximum test-isolation. Applied to all tests
     """
-    # janitor_engine = create_async_engine(TEST_SQLALCHEMY_DATABASE_URL)
-    janitor_engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
-    async with janitor_engine.begin() as conn:
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
-    async with janitor_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-    await janitor_engine.dispose()
+    await engine.dispose()
